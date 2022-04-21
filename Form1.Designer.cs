8af1db6822cababb631eba4cac2b2023f06feff8
robandrade <103383943+robandrade@users.noreply.github.com>
2022-04-21 00:08:16 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace MileStoneProject
+{
+    partial class Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inventoryListBox = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.addInvtry = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inventoryListBox
+            // 
+            this.inventoryListBox.FormattingEnabled = true;
+            this.inventoryListBox.Items.AddRange(new object[] {
+            "#000001 Blue Jersey ($10.00)",
+            "#000002 Red Jersey ($10.00)"});
+            this.inventoryListBox.Location = new System.Drawing.Point(13, 51);
+            this.inventoryListBox.Name = "inventoryListBox";
+            this.inventoryListBox.Size = new System.Drawing.Size(299, 160);
+            this.inventoryListBox.TabIndex = 8;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(536, 77);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(536, 48);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(215, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(173, 25);
+            this.label.TabIndex = 12;
+            this.label.Text = "Soccer Inventory";
+            // 
+            // addInvtry
+            // 
+            this.addInvtry.Location = new System.Drawing.Point(335, 51);
+            this.addInvtry.Name = "addInvtry";
+            this.addInvtry.Size = new System.Drawing.Size(181, 20);
+            this.addInvtry.TabIndex = 13;
+            // 
+            // Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 248);
+            this.Controls.Add(this.addInvtry);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.inventoryListBox);
+            this.Name = "Inventory";
+            this.Text = "Inventory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox inventoryListBox;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox addInvtry;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MileStoneProject
+{
+    public partial class Inventory : Form
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+
+        public string AddInventory(string inventory)
+        {
+            return inventory;
+        }
+       
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string userInput = addInvtry.Text;
+            string add;
+
+            add = AddInventory(userInput);   
+            
+            inventoryListBox.Items.Add(add);  
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string selectedInventory;
+
+            selectedInventory = (string)inventoryListBox.SelectedItem;
+            
+            inventoryListBox.Items.Remove(selectedInventory);
+            
+        }
+    }
+}
